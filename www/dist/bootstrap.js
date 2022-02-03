bf003568cb804b49c07f702dd212ee9cd14fca15
Your Name <slagoonisdune@gmail.com>
2022-02-02 21:46:19 -0600

    added dist/ to .gitignore, as it can be just built from *npm run build*

--- www/dist/bootstrap.js
+++
@@ -1,4 +0,0 @@
-// A dependency graph that contains any wasm must all be imported
-// asynchronously. This `bootstrap.js` file does the single async import, so
-// that no one else needs to worry about it again.
-Promise.resolve().then(function () { return require("./index.js"); }).catch(function (e) { return console.error("Error importing `index.js`:", e); });
