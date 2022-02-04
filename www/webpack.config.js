52dcebd0477962370624b97ce96ce85050613d9d
Your Name <slagoonisdune@gmail.com>
2022-02-03 18:38:49 -0600

    fixed. now runs but with problems loading style.css

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,6 +1,7 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
+
 module.exports = {
   entry: "./bootstrap.ts",
   output: {
@@ -9,6 +10,15 @@ module.exports = {
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "index.html") }
+      ]
+    })
   ],
+  // CRITICAL
+  experiments: {
+    //asyncWebAssembly: true,
+    syncWebAssembly: true,
+  },
 };
